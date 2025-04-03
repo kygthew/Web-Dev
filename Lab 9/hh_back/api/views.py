daa58221279da80a472ccf8b88ab94363ab0aac4
Kamila <erlankamila05@gmail.com>
2025-04-04 02:10:50 +0500

    added lab9

---
+++ Lab 9/hh_back/api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Company, Vacancy
+from .serializers import CompanySerializer, VacancySerializer
+
+#Company Views
+class CompanyListView(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class CompanyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class VacanciesByCompanyView(generics.ListAPIView):
+    serializer_class = VacancySerializer
+
+    def get_queryset(self):
+        company_id = self.kwargs['id']
+        return Vacancy.objects.filter(company_id=company_id)
+
+#Vacancy Views
+class VacancyListView(generics.ListCreateAPIView):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+
+class VacancyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+
+class TopTenVacanciesView(generics.ListAPIView):
+    serializer_class = VacancySerializer
+
+    def get_queryset(self):
+        return Vacancy.objects.order_by('-salary')[:10]  # Top 10 by salary
